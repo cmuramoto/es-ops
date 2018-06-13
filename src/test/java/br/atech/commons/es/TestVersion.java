83b6ad8e42a0115a3197653ede3053f4074e615e
cmuramoto <cleber.muramoto@gmail.com>
2018-06-13 15:29:30 -0300

    src

--- src/test/java/br/atech/commons/es/TestVersion.java
+++ src/test/java/br/atech/commons/es/TestVersion.java
@@ -2,11 +2,14 @@ package br.atech.commons.es;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.nc.es.api.IElasticSearchOps;
+import com.nc.es.api.NodeInfo;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = RestClientConfig.class)
@@ -17,8 +20,11 @@ public class TestVersion {
 
 	@Test
 	public void run() {
-		System.out.println(ops.info());
+		Logger log = LoggerFactory.getLogger(getClass());
+		NodeInfo info = ops.info();
+		log.info("{}", info);
 
-		System.out.println(ops.supportsTypes());
+		boolean st = ops.supportsTypes();
+		log.info("{}", st);
 	}
 }
